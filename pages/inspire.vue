d404ff32b534c92884550205470baea9851c50e6
silvesterwali <silvesterlhwali123@gmail.com>
2021-08-19 21:57:44 +0800

    first init

---
+++ pages/inspire.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <h1></h1>
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
---
+++ pages/login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <v-card>
+      <v-card-title>Hello</v-card-title>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'hello',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
