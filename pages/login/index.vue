780df489d8a3b0df39a33db17416bf787be847f6
silvesterwali <silvesterlhwali123@gmail.com>
2021-08-19 22:09:06 +0800

    remove some default stuff

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
-    <h1>Hello</h1>
-    <v-card>
-      <v-card-title>Hello</v-card-title>
-    </v-card>
+    <v-row>
+      <v-col class="text-center">
+        <h1></h1>
+        <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+        <blockquote class="blockquote">
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      message: 'hello',
-    }
-  },
-}
+export default {}
 </script>
 
 <style lang="scss" scoped></style>
